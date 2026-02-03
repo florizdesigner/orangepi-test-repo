3b41f181cb3b3dcbf39e21ae2627e619e09aee3e
vsokolov <vsokolov@yoomoney.ru>
2026-02-03 15:42:42 +0300

    edit

--- joystick_test.py
+++ joystick_test.py
@@ -2,15 +2,17 @@
 import RPi.GPIO as GPIO
 import time
 
-# Настройка пинов (измените под свои)
+# Физические пины
 UP = 40
 DOWN = 38
 LEFT = 37
 RIGHT = 36
 MID = 35
+RST = 33
+SET = 31
 
-# Настройка GPIO
-GPIO.setmode(GPIO.BCM)
+# ВАЖНО: используем BOARD режим для физической нумерации
+GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
 GPIO.setup(UP, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -18,6 +20,8 @@ GPIO.setup(DOWN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(LEFT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(RIGHT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(MID, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(RST, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(SET, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 print("Тест джойстика запущен. Нажмите Ctrl+C для выхода.")
 print("-" * 40)
@@ -42,6 +46,14 @@ try:
             
         if GPIO.input(MID) == GPIO.LOW:
             print("🔘 КНОПКА НАЖАТА")
+            time.sleep(0.2)
+            
+        if GPIO.input(RST) == GPIO.LOW:
+            print("🔄 RST НАЖАТА")
+            time.sleep(0.2)
+            
+        if GPIO.input(SET) == GPIO.LOW:
+            print("⚙️  SET НАЖАТА")
             time.sleep(0.2)
             
         time.sleep(0.05)
