f690eb2aa9d528e2992a875fd6b11340b3e7bfbc
Семен В. Сергеев <svsergeev@yoomoney.ru>
2026-02-11 13:41:18 +0300

    Added lazy screen init

--- manager/screen_manager.py
+++ manager/screen_manager.py
@@ -2,6 +2,8 @@ class ScreenManager:
     def __init__(self, ui):
         self.ui = ui
         self.screens = {}
+        # Кеш готовых инстансов экранов, чтобы не дублировать подписки на события
+        self._instances = {}
         self.current = None
 
     def register(self, name, screen_cls):
@@ -10,7 +12,13 @@ class ScreenManager:
     def set(self, name):
         if self.current:
             self.current.on_exit()
-        self.current = self.screens[name](self.ui, self)
+
+        # Лениво создаём экран один раз и переиспользуем его,
+        # чтобы __init__ (и подписки на шину событий) не вызывались многократно.
+        if name not in self._instances:
+            self._instances[name] = self.screens[name](self.ui, self)
+
+        self.current = self._instances[name]
         self.current.on_enter()
 
     def draw(self):
